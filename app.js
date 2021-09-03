35cd1eb8b8062150baaa9969f9aac18bfa6bfdc4
Taylor Laury <tlaury6@yahoo.com>
2021-09-03 12:33:39 -0400

    Created JS Operators Exercise

--- app.js
+++ app.js
@@ -6,14 +6,47 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var dividing = a / b;
+console.log(add); // outputs -> 24
+console.log(minus); // outputs -> 16
+console.log(multiply); // outputs -> 80
+console.log(dividing); // outputs -> 5
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+console.log(a + str); // outputs -> 1111
+console.log(a + str2); // outputs -> 11eleven
+console.log(a + isPresent); // outputs -> 12
+console.log(a + firstName); // outputs -> 11Jackie
+console.log(a + lastName); // outputs -> 11Chan
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let a = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+let firstName = "Robin";
+let lastName = "Williams";
+console.log(a == str); // output -> true
+console.log(a === str); // output ->false
+console.log(!isPresent); // output -> true
+console.log("eleven" == str2 && a >= str); // output -> false
+console.log(!isPresent || isPresent); // output -> true
+console.log(0 == false); // output -> true
+console.log(0 === false); // output -> false
+console.log(0 != false); // output -> false
+console.log(0 !==false); // output -> true
